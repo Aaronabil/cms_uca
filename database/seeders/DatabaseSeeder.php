c13a8e42fd6f391f244ce597db9eca9ad888db1f
ophari <ophari10@gmail.com>
2026-01-10 11:00:41 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,6 +33,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SiteSettingSeeder::class,
             FacultyStudyProgramSeeder::class,
+            KaprodiSeeder::class,
             MenuSeeder::class,
             PageSeeder::class,
             ArtikelSeeder::class,
