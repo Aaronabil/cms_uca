d56c93726884e40234c996569ff0c21c927b0433
ophari <ophari10@gmail.com>
2026-01-10 20:32:49 +0700

    fix button

--- resources/views/filament/resources/site-settings/pages/manage-site-settings.blade.php
+++ resources/views/filament/resources/site-settings/pages/manage-site-settings.blade.php
@@ -2,8 +2,6 @@
     <form wire:submit="submit" class="space-y-6">
         {{ $this->form }}
 
-        <x-filament::actions 
-            :actions="$this->getFormActions()"
-        />
+
     </form>
 </x-filament-panels::page>
